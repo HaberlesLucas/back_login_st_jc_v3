1e47586c7503528b7b2f40d5b19e29b13798f277
Lucas <lucashaberles41q@gmail.com>
2024-11-03 10:14:20 -0300

    ultimo commit antes de fusionar proyectos

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 Route::group([
     'middleware' => 'api',
     'prefix' => 'v3',
@@ -19,9 +16,6 @@ Route::group([
     Route::get('/reset-password/{token}', [UserController::class, 'showResetForm']);
     Route::POST('/reset-password', [UserController::class, 'updatePasswordReset']);
 
-    //restablecer contraseña - fin 
-
-
     // Rutas protegidas con autenticación
     Route::middleware('auth:api')->group(function () {
         Route::get('/get-user-info', [UserController::class, 'getLoggedInUser']); // Ruta protegida
@@ -30,10 +24,6 @@ Route::group([
         Route::POST('/logout', [AuthController::class, 'logout']);
         Route::POST('/refresh', [AuthController::class, 'refresh']);
 
-
-
-        // update-user-name
-        // update-user-password
         Route::PUT('/update-user-name', [UserController::class, 'updateName']);
         Route::PUT('/update-user-password', [UserController::class, 'updatePassword']);
 
@@ -47,43 +37,3 @@ Route::group([
         Route::get('/check-correo/{correo}', [UserController::class, 'checkCorreoExists']);
     });
 });
-
-
-
-
-
-
-
-
-// Route::group([
-//     'middleware' => 'api',
-//     'prefix' => 'v3',
-// ], function () {
-//     Route::POST('/login', [AuthController::class, 'login']);
-    
-//     Route::get('/get-user-info', [UserController::class, 'getLoggedInUser'])->middleware('auth:api');
-
-//     Route::POST('/register', [AuthController::class, 'setUsuario']);
-//     Route::get('/', [UserController::class, 'getUsuarios']);
-//     Route::get('/{dni}', [UserController::class, 'getUsuario']);
-//     Route::PUT('/restore/{dni}', [UserController::class, 'restoreUsuario']);
-//     Route::DELETE('/{dni}', [UserController::class, 'deleteUsuario']);
-//     Route::PUT('/{dni}', [UserController::class, 'updateUsuario']);
-//     Route::get('/check-dni/{dni}', [UserController::class, 'checkDniExists']);
-
-
-//     Route::POST('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-//     Route::POST('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
-
-// });
-
-
-
-
-
-
-// Route::middleware('auth:api')->get('/get-user-info', [UserController::class, 'getLoggedInUser']);
-
-// Route::get('/get-user-info', function () {
-//     echo ("hola");
-// });
